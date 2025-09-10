418a857a191c627a167616b9f551770352a58549
mjake09 <jake.reuma@authorstranquilitypress.com>
2025-09-11 04:38:09 +0800

    update

--- app/page.js
+++ app/page.js
@@ -8,10 +8,10 @@ export default function Home() {
     <div className="min-h-screen bg-blue-50 flex items-center justify-center">
       <div className='text-center'>
       <h1 className='text-4xl font-bold text-blue-900 mb-4'>
-        Welcome to my website pre
+        Welcome to my website
       </h1>
       <p className='text-xl text-gray-700 mb-8'>
-        birthday ni lodi D
+        This is my first web app
       </p>
       {clicked ? (
         <p className='text-2xl text-green-600 font-bold'>
